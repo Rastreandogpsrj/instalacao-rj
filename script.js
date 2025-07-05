48b92cbde473ae5b195301b6f22456a345e38ba3
Rastreandogpsrj <rastreandogpsbr@gmail.com>
2025-07-04 22:54:21 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+async function verValor() {
+  const input = document.getElementById("bairroInput").value.trim();
+  const popup = document.getElementById("popup");
+  const whatsapp = document.getElementById("whatsapp");
+
+  const response = await fetch("https://instalacao-por-bairro-gps.vercel.app/bairros.json");
+  const data = await response.json();
+
+  const normalizar = str =>
+    str.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toLowerCase().trim();
+
+  const resultado = data.find(entry => normalizar(entry.bairro.trim()) === normalizar(input));
+
+  popup.style.display = "block";
+  whatsapp.style.display = "block";
+
+  if (resultado) {
+    popup.innerHTML = `✅ Sua instalação é apenas R$ ${resultado.valor}`;
+  } else {
+    popup.innerHTML = `❌ Este bairro não foi encontrado.<br>📌 Verifique se você digitou corretamente o nome do bairro.`;
+  }
+}
